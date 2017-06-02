dbe09babe9594290fda0383f0728c2e5eb6a9aec
lavrs <lavrentev.srg@gmail.com>
2017-06-02 17:01:18 +0300

    nodes cooperation in progress

--- blockchain.go
+++ blockchain.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"crypto/sha256"
-	"errors"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"golang.org/x/net/websocket"
+	"log"
 	"math/rand"
+	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
 var (
 	tmpBlk     *Block
 	blockchain []*Block
+
+	iPeer    = *flag.String("iperr", "", "init peer address")
+	wsPort   = *flag.String("hport", "", "set http port")
+	httpPort = *flag.String("wsport", "", "set ws port")
+	nodes    []websocket.Addr
+
+	successMineNotify = make(chan *Block)
 )
 
 type Block struct {
@@ -20,7 +34,7 @@ type Block struct {
 	prevHash  string
 	hash      string
 	timestamp time.Time
-	fact      []*interface{}
+	facts     []*interface{}
 	task      *Task
 }
 
@@ -30,13 +44,9 @@ type Task struct {
 	complexity int
 }
 
-type Transaction struct {
-	amount float64
-	from   string
-	to     string
-}
-
 func init() {
+	flag.Parse()
+
 	rand.Seed(time.Now().UnixNano())
 
 	blk := &Block{
@@ -59,7 +69,7 @@ func init() {
 
 func (b *Block) String() string {
 	return b.prevHash + b.timestamp.String() +
-		fmt.Sprint(b.fact, b.index, b.task.end, b.task.start, b.task.complexity)
+		fmt.Sprint(b.facts, b.index, b.task.end, b.task.start, b.task.complexity)
 }
 
 func calcHash(b *Block) string {
@@ -98,23 +108,6 @@ func mine(decision string) bool {
 }
 
 func addFact(data *interface{}) {
-	tmpBlk.fact = append(tmpBlk.fact, data)
-}
-
-func fact(index int) ([]*interface{}, error) {
-	if index >= len(blockchain) || index < 0 {
-		return nil, errors.New("invalid block index")
-	}
-	return blockchain[index].fact, nil
-}
-
-func facts() (i []*interface{}) {
-	for _, block := range blockchain {
-		for _, bt := range block.fact {
-			i = append(i, bt)
-		}
-	}
-	return
 }
 
 func isValidBlock(nBlock, pBlock *Block) bool {
@@ -146,4 +139,114 @@ func generateTask() *Task {
 		end:        end,
 		complexity: rand.Intn(end) + start,
 	}
+}
+
+func main() {
+	done := make(chan os.Signal)
+	defer close(done)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	// http server
+	go func() {
+		http.HandleFunc("/blocks", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(blockchain))
+		})
+
+		http.HandleFunc("/facts", func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case http.MethodGet:
+				var facts string
+				for _, block := range blockchain {
+					facts += fmt.Sprint(block.facts)
+				}
+
+				w.WriteHeader(http.StatusOK)
+				w.Write([]byte(facts))
+			case http.MethodPost:
+				var (
+					buf  = make([]byte, 10240)
+					fact interface{}
+				)
+				n, err := r.Body.Read(buf)
+				if err != nil {
+					w.WriteHeader(http.StatusBadRequest)
+					break
+				}
+				err = json.Unmarshal(buf[:n], fact)
+				tmpBlk.facts = append(tmpBlk.facts, &fact)
+				w.WriteHeader(http.StatusOK)
+			}
+		})
+
+		http.HandleFunc("/mine", func(w http.ResponseWriter, r *http.Request) {
+			mine(r.URL.Query().Get("decision"))
+		})
+
+		http.HandleFunc("/nodes", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(nodes))
+		})
+
+		log.Fatal(http.ListenAndServe(":"+httpPort, nil))
+	}()
+
+	// websocket server
+	go func() {
+		http.Handle("/peer", websocket.Handler(func(ws *websocket.Conn) {
+			go func() {}()
+			go func() {}()
+		}))
+
+		log.Fatal(http.ListenAndServe(":"+wsPort, nil))
+	}()
+
+	// client
+	r, err := http.Get(iPeer + "/nodes")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	err = json.NewDecoder(r.Body).Decode(nodes)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r, err = http.Get(iPeer + "/blocks")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer r.Body.Close()
+
+	err = json.NewDecoder(r.Body).Decode(blockchain)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, node := range nodes {
+		go func() {
+			ws, err := websocket.Dial(node.String()+"peer", "", "http://localhost")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			for {
+				blk, ok := <-successMineNotify
+				if ok {
+					blkjson, err := json.Marshal(blk)
+					if err != nil {
+						log.Fatal(err)
+					}
+
+					_, err = ws.Write(blkjson)
+					if err != nil {
+						log.Fatal(err)
+					}
+				}
+			}
+		}()
+	}
+
+	<-done
 }
